441b893d5ea434b265835c2c2692fdd1f85ab601
khris-xp <khrisbharmmano@gmail.com>
2024-04-25 23:56:17 +0700

    fix: update unit test

--- controllers/tax_controller_test.go
+++ controllers/tax_controller_test.go
@@ -15,25 +15,129 @@ func TestCalculateTax(t *testing.T) {
 	tests := []struct {
 		name        string
 		totalIncome float64
-		statusCode  int
+		wht         float64
+		allowances  []struct {
+			allowanceType string
+			amount        float64
+		}
+		statusCode int
 	}{
-		{"StatusBadRequest when Income -10", -10.0, http.StatusBadRequest},
-		{"StatusOK when Income 0", 0.0, http.StatusOK},
-		{"StatusOK when Income 70,000", 70000.0, http.StatusOK},
-		{"StatusOK when Income 150,000", 150000.0, http.StatusOK},
-		{"StatusOK when Income 300,000", 300000.0, http.StatusOK},
-		{"StatusOK when Income 500,000", 500000.0, http.StatusOK},
-		{"StatusOK when Income 750,000", 750000.0, http.StatusOK},
-		{"StatusOK when Income 1,000,000", 1000000.0, http.StatusOK},
-		{"StatusOK when Income 1,500,000", 1500000.0, http.StatusOK},
-		{"StatusOK when Income 2,000,000", 2000000.0, http.StatusOK},
-		{"StatusOK when Income 2,500,000", 2500000.0, http.StatusOK},
+		{
+			"StatusOK with allowances",
+			70000.0,
+			2000.0,
+			[]struct {
+				allowanceType string
+				amount        float64
+			}{
+				{
+					"donation",
+					0.0,
+				},
+			},
+			http.StatusOK,
+		},
+		{"StatusOK when Income 70,000", 70000.0, 0.0, []struct {
+			allowanceType string
+			amount        float64
+		}{
+			{
+				"donation",
+				0.0,
+			},
+		}, http.StatusOK},
+		{"StatusOK when Income 150,000", 150000.0, 0.0, []struct {
+			allowanceType string
+			amount        float64
+		}{
+			{
+				"donation",
+				0.0,
+			},
+		}, http.StatusOK},
+		{"StatusOK when Income 300,000", 300000.0, 0.0, []struct {
+			allowanceType string
+			amount        float64
+		}{
+			{
+				"donation",
+				0.0,
+			},
+		}, http.StatusOK},
+
+		{"StatusOK when Income 70,000, WHT 10,000", 70000.0, 10000.0, []struct {
+			allowanceType string
+			amount        float64
+		}{
+			{
+				"donation",
+				0.0,
+			},
+		}, http.StatusOK},
+		{"StatusOK when Income 150,000, WHT 10,000", 150000.0, 10000.0, []struct {
+			allowanceType string
+			amount        float64
+		}{
+			{
+				"donation",
+				0.0,
+			},
+		}, http.StatusOK},
+
+		{"StatusOK when Income MaxFloat64, WHT MaxFloat64", 1.7976931348623157e+308, 1.7976931348623157e+308, []struct {
+			allowanceType string
+			amount        float64
+		}{
+			{
+				"donation",
+				0.0,
+			},
+		}, http.StatusOK},
+		{"StatusOK when Income 0.5, WHT 0.1", 0.5, 0.1, []struct {
+			allowanceType string
+			amount        float64
+		}{
+			{
+				"donation",
+				0.0,
+			},
+		}, http.StatusOK},
+
+		{"StatusBadRequest when Income MinFloat64, WHT MinFloat64", -1.7976931348623157e+308, -1.7976931348623157e+308, []struct {
+			allowanceType string
+			amount        float64
+		}{
+			{
+				"donation",
+				0.0,
+			},
+		}, http.StatusBadRequest},
+
+		{"StatusBadRequest when Income -10", -10.0, 0.0, []struct {
+			allowanceType string
+			amount        float64
+		}{
+			{
+				"donation",
+				0.0,
+			},
+		}, http.StatusBadRequest},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := echo.New()
-			req := httptest.NewRequest(http.MethodPost, "/tax/calculations", strings.NewReader(fmt.Sprintf(`{"totalIncome": %v}`, tt.totalIncome)))
+			var allowancesJSON string
+			if len(tt.allowances) > 0 {
+				var allowanceStrings []string
+				for _, allowance := range tt.allowances {
+					allowanceStrings = append(allowanceStrings, fmt.Sprintf(`{"allowanceType": "%s", "amount": %v}`, allowance.allowanceType, allowance.amount))
+				}
+				allowancesJSON = fmt.Sprintf(`"allowances": [%s], `, strings.Join(allowanceStrings, ","))
+			}
+
+			reqBody := fmt.Sprintf(`{%s"totalIncome": %v, "wht": %v}`, allowancesJSON, tt.totalIncome, tt.wht)
+			req := httptest.NewRequest(http.MethodPost, "/tax/calculations", strings.NewReader(reqBody))
 			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 			rec := httptest.NewRecorder()
 			c := e.NewContext(req, rec)
@@ -42,12 +146,11 @@ func TestCalculateTax(t *testing.T) {
 			err := tCl.CalculateTax(c)
 
 			if (err != nil && tt.statusCode != http.StatusBadRequest) || rec.Code != tt.statusCode {
-				t.Errorf("%s failed. Income: %v, Expected Status Code: %d, Got: %d, Error: %v",
-					tt.name, tt.totalIncome, tt.statusCode, rec.Code, err)
+				fmt.Printf(color.RedString("%s failed. Expected status code: %v, got: %v\n"), tt.name, tt.statusCode, rec.Code)
 				return
 			}
 
-			fmt.Printf(color.GreenString("%s passed. Income: %v\n"), tt.name, tt.totalIncome)
+			fmt.Printf(color.GreenString("%s passed. Income: %v, WHT: %v\n"), tt.name, tt.totalIncome, tt.wht)
 		})
 	}
 }
